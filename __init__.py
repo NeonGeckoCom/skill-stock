00db8d91e92c2412508d9290af411af51430da82
Daniel McKnight <34697904+NeonDaniel@users.noreply.github.com>
2022-05-19 18:41:35 -0700

    Skill Refactor and Unit Tests (#11)
    * Refactor resources into locale directory
    Cleanup skill class for settings compat
    Add initial unit tests
    
    * Remove requests dependency
    Annotate future improvements
    
    Co-authored-by: Daniel McKnight <daniel@neon.ai>

--- __init__.py
+++ __init__.py
@@ -25,27 +25,11 @@
 # LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
 # NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 # SOFTWARE,  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-#
-# Copyright 2018 Mycroft AI Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 
-import requests
-
-from time import sleep
-# from adapt.intent import IntentBuilder
-# from mycroft.skills import intent_handler
 from neon_utils.skills.neon_skill import NeonSkill, LOG
+from neon_api_proxy.client import alpha_vantage
+
+from mycroft.skills import intent_file_handler
 
 
 class StockSkill(NeonSkill):
@@ -58,29 +42,35 @@ class StockSkill(NeonSkill):
                              "coca-cola": "ko",
                              "google": "goog",
                              "exxonmobil": "xom"}
+        self._service = None
 
-        self.service = self.settings.get('service') or "Alpha Vantage"
-        self.api_key = self.settings.get('api_keys', {}).get(self.service) or self.settings.get("api_key")
+    @property
+    def service(self):
+        if not self._service:
+            service = self.settings.get('service') or "Alpha Vantage"
+            if service == "Financial Modeling Prep" and not self.api_key:
+                service = "Alpha Vantage"
+            self._service = service
+        return self._service
 
-        if self.service == "Financial Modeling Prep" and not self.api_key:
-            self.service = "Alpha Vantage"
-            self.update_skill_settings({"service": self.service}, skill_global=True)
+    @property
+    def api_key(self):
+        return self.settings.get('api_keys', {}).get(self.service) or \
+               self.settings.get("api_key")
 
-        if self.service == "Alpha Vantage":
-            from neon_utils.service_apis import alpha_vantage
-            self.data_source = alpha_vantage
+    @property
+    def data_source(self):
+        return alpha_vantage
 
-    def initialize(self):
-        if self.data_source:
-            self.register_intent_file("StockPrice.intent", self.handle_stock_price_intent)
-        else:
-            LOG.error(f"No sata provider specified; skill will be disabled!")
-
-    # @intent_handler(IntentBuilder("").require("StockPriceKeyword").require("Company"))
-    def handle_stock_price_intent(self, message):
-        company = message.data.get("company")
+    @intent_file_handler("StockPrice.intent")
+    def handle_stock_price(self, message):
+        """
+        Handle a query for stock value
+        """
+        company = message.data.get("company").lower()
         LOG.debug(company)
 
+        # TODO: Generalize parsing for language support
         # Filter out articles from company name
         if str(company).split()[0] in ["of", "for", "what"]:
             LOG.warning('fixing string')
@@ -95,21 +85,21 @@ class StockSkill(NeonSkill):
             company = company.strip().rstrip(" stock")
 
         try:
-            # Special case handling for 3m
-            if company == 'm' and "3m" in str(message.data['utterance']).split():
-                company = "mmm"
+            # # Special case handling for 3m
+            # if company == 'm' and "3m" in str(message.data['utterance']).split():
+            #     company = "mmm"
 
-            # Special case handling for common brands that don't match accurately
-            if company in self.translate_co.keys():
+            # Special case for common stocks that don't match accurately
+            if company in self.translate_co:
                 company = self.translate_co[company]
                 LOG.debug(company)
 
-            match_data = self.search_company(company)
+            match_data = self._search_company(company)
             company = match_data.get("name")
             symbol = match_data.get("symbol")
             LOG.debug(f"found {company} with symbol {symbol}")
             if symbol:
-                quote = self.get_stock_price(symbol)
+                quote = self._get_stock_price(symbol)
             else:
                 quote = None
             if not all([symbol, company, quote]):
@@ -119,24 +109,17 @@ class StockSkill(NeonSkill):
                             'company': company,
                             'price': quote,
                             'provider': self.service}
-                self._mark_1_info_on_speech(response['symbol'], response['price'])
                 self.speak_dialog("stock.price", data=response)
                 if self.gui_enabled:
                     self.gui["title"] = company
                     self.gui["text"] = f"${quote}"
                     self.gui.show_page("Stock.qml")
                     self.clear_gui_timeout()
-            sleep(12)
-            self._mark_1_display_release()
-
-        except requests.HTTPError as e:
-            self.speak_dialog("api.error", data={'error': str(e)})
         except Exception as e:
-            self.log.exception(e)
-            if self.voc_match(message.data["utterance"], "StockPriceKeyword"):
-                self.speak_dialog("not.found", data={'company': company})
+            LOG.exception(e)
+            self.speak_dialog("not.found", data={'company': company})
 
-    def search_company(self, company: str) -> dict:
+    def _search_company(self, company: str) -> dict:
         """
         Find a traded company by name
         :param company: Company to search
@@ -147,9 +130,10 @@ class StockSkill(NeonSkill):
             kwargs["api_key"] = self.api_key
         stocks = self.data_source.search_stock_by_name(company, **kwargs)
         LOG.debug(f"stocks={stocks}")
+        # TODO: Catch and warn on API error here
         return stocks[0]
 
-    def get_stock_price(self, symbol: str):
+    def _get_stock_price(self, symbol: str):
         """
         Get the current trade price by ticker symbol
         :param symbol: Ticker symbol to query
@@ -159,26 +143,10 @@ class StockSkill(NeonSkill):
         if self.api_key:
             kwargs["api_key"] = self.api_key
         stock_data = self.data_source.get_stock_quote(symbol)
+        # TODO: Catch and warn on API error here
         if not stock_data.get("price"):
             return None
         return str(round(float(stock_data.get("price")), 2))
-
-    def _mark_1_info_on_speech(self, symbol, price):
-        """
-        Show the ticker symbol and price on the Mark-1 display when speaking
-        """
-        # When speech starts, output the information on the Mark-1 display
-        self.bus.once("recognizer_loop:audio_output_start",
-                      self.enclosure.mouth_text("{}: {}".format(symbol, price))
-                      )
-        self.enclosure.deactivate_mouth_events()
-
-    def _mark_1_display_release(self):
-        """
-        Reset Mark-1 display if it was taken by the skill.
-        """
-        self.enclosure.activate_mouth_events()
-        self.enclosure.mouth_reset()
 
 
 def create_skill():
